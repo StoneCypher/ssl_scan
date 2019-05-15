a417dfa8f7d7cb366f14a5d03bea501f0a0bae25
John Haugeland <stonecypher@gmail.com>
2019-05-15 10:15:03 -0700

    help shows - wrong help, but

--- src/js/cli.js
+++ src/js/cli.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
-console.log('woo');
+const arghelp = require('./cli_arg_help.js'),
+      cli     = require('command-line-args')(arghelp.args),
+      clu     = require('command-line-usage'),
+      ssl_c   = require('ssl-checker');
+
+
+
+if (cli.help !== undefined) {  // because it's null in 5, sigh
+
+  console.log(clu(arghelp.sections));
+
+} else {
+
+
+
+}
